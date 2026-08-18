699cde55ec9955082491ef135fba95da243ddab5
Ruan Lopes <rpo.lopes@hotmail.com>
2026-08-18 00:38:55 -0300

    [panel] feat: tag merging

--- backend/Services/ITagService.cs
+++ backend/Services/ITagService.cs
@@ -1,7 +1,11 @@
+using AudioArchive.Database.Entity;
+
 namespace AudioArchive.Services
 {
   public interface ITagService
   {
     Task<int> CleanupOrphanTags();
+    Task<List<Tag>> GetTags();
+    Task<int> MergeTags(Guid sourceTagId, Guid targetTagId);
   }
 }
--- backend/Services/TagService.cs
+++ backend/Services/TagService.cs
@@ -1,4 +1,6 @@
 using AudioArchive.Database;
+using AudioArchive.Database.Entity;
+using AudioArchive.Shared;
 using Microsoft.EntityFrameworkCore;
 
 namespace AudioArchive.Services
@@ -16,6 +18,47 @@ namespace AudioArchive.Services
         ) orphans
         WHERE t."Id" = orphans."Id";
       """);
+    }
+
+    public async Task<List<Tag>> GetTags() {
+      return await database.Tags.AsNoTracking().OrderBy(t => t.Name).ToListAsync();
+    }
+
+    public async Task<int> MergeTags(Guid sourceTagId, Guid targetTagId) {
+      if (sourceTagId == targetTagId)
+        throw new BadRequestException("Source and target tags must be different.", sourceTagId.ToString());
+
+      if (!await database.Tags.AnyAsync(t => t.Id == sourceTagId))
+        throw new NotFoundException("Could not find source tag.", sourceTagId.ToString());
+
+      if (!await database.Tags.AnyAsync(t => t.Id == targetTagId))
+        throw new NotFoundException("Could not find target tag.", targetTagId.ToString());
+
+      await using var transaction = await database.Database.BeginTransactionAsync();
+
+      await database.Database.ExecuteSqlRawAsync("""
+        INSERT INTO audio_metadata_tags ("AudioMetadatasId", "TagsId")
+        SELECT amt."AudioMetadatasId", {0}
+        FROM audio_metadata_tags amt
+        WHERE amt."TagsId" = {1}
+          AND NOT EXISTS (
+            SELECT 1 FROM audio_metadata_tags t2
+            WHERE t2."AudioMetadatasId" = amt."AudioMetadatasId"
+              AND t2."TagsId" = {0}
+          );
+      """, targetTagId, sourceTagId);
+
+      var affected = await database.Database.ExecuteSqlRawAsync("""
+        DELETE FROM audio_metadata_tags WHERE "TagsId" = {0};
+      """, sourceTagId);
+
+      await database.Database.ExecuteSqlRawAsync("""
+        DELETE FROM tags WHERE "Id" = {0};
+      """, sourceTagId);
+
+      await transaction.CommitAsync();
+
+      return affected;
     }
   }
 }
